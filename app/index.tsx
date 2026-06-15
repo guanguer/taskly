c7b0ad9788972d94699848e8795dc7346e1862c4
guanguer <jftabord@gmail.com>
2026-06-14 19:20:49 -0700

    Add TextInput field and dynamic shopping list logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,51 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
+
 export default function App() {
+  const [value, setValue] = useState("");
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList: ShoppingListItemType[] = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Cappuccino" isCompleted />
-      <ShoppingListItem name="Latte" />
-      <ShoppingListItem name="Mocha" />
+      <TextInput
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item: ShoppingListItemType) => (
+        <ShoppingListItem
+          key={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+        />
+      ))}
     </View>
   );
 }
@@ -16,11 +54,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
   },
-  link: {
-    textAlign: "center",
-    marginBottom: 18,
-    fontSize: 24,
+  textInput: {
+    borderColor: theme.colorLightGray,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
