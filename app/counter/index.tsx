fe96fa567f9d79caac74c7912b55e841f33daf31
guanguer <jftabord@gmail.com>
2026-06-15 18:20:48 -0700

    Add local scheduled push notification

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,39 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import * as Notifications from "expo-notifications";
 import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function ConterScreen() {
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app!  📨",
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5,
+        },
+      });
+    } else {
+      Alert.alert(
+        "Unable to schedule a notification",
+        "Enable the notification permission for Expo Go in settings",
+      );
+    }
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={8}
+        onPress={scheduleNotification}
+      >
+        <Text style={styles.buttonText}>Schedule Notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,7 +45,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: theme.colorWhite,
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
